a2e296975f9441b2947ab55db36fa129f775e5c0
Flavio de Vasconcellos Correa <96501461+sheep-farm@users.noreply.github.com>
2026-05-20 10:46:40 -0300

    Create CTestTestfile.cmake

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/flavop/GitHub/ratmoney
+# Build directory: /home/flavop/GitHub/ratmoney
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[all_tests]=] "/home/flavop/GitHub/ratmoney/test_bin")
+set_tests_properties([=[all_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/flavop/GitHub/ratmoney/CMakeLists.txt;56;add_test;/home/flavop/GitHub/ratmoney/CMakeLists.txt;0;")
